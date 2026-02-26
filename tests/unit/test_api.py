fea24f1b090de1687d2bf0829239b76f430a1876
Andrii Nechaiev <andreynech@aswsyst.cz>
2026-02-26 07:07:41 +0000

    24: better URLs concatenation

--- tests/unit/test_api.py
+++ tests/unit/test_api.py
@@ -34,10 +34,42 @@ def test_url_without_endpoint(endpoint: typing.Optional[str]):
 
     The base URL must be used.
     """
-    base = "http://fake.com"
+    base = "http://fake.com/"
     comps = build_api(
         base_url=base,
         http_client=FakeHTTPClient(responses=[HTTPResponse(200, b"", {})]),
     )
     comps.api.make_call(endpoint=endpoint)
     assert comps.http_client.history[0]["url"] == base
+
+
+@pytest.mark.parametrize(
+    ["base_url", "endpoint", "result_url"],
+    [
+        ("http://fake.com", "endpoint", "http://fake.com/endpoint"),
+        ("http://fake.com", "/endpoint", "http://fake.com/endpoint"),
+        ("http://fake.com/api", "endpoint", "http://fake.com/api/endpoint"),
+        ("http://fake.com/api", "/endpoint", "http://fake.com/api/endpoint"),
+        ("http://fake.com/api/", "endpoint", "http://fake.com/api/endpoint"),
+        ("http://fake.com/api/", "/endpoint", "http://fake.com/api/endpoint"),
+    ],
+)
+def test_base_url_and_endpoint_combinations(
+    base_url: str, endpoint: str, result_url: str
+):
+    """Test base_url + endpoint combinations."""
+    comps = build_api(
+        base_url=base_url,
+        http_client=FakeHTTPClient(responses=[HTTPResponse(200, b"", {})]),
+    )
+    comps.api.make_call(endpoint=endpoint)
+
+    assert comps.http_client.history == [
+        {
+            "_method": "_request",
+            "headers": {"h": "v"},
+            "json": {"k": "v"},
+            "method": "POST",
+            "url": result_url,
+        },
+    ]
